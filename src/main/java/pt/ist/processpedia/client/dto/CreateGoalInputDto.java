bd199458ffe8a1af861876ed1980b017e486c67f
David Martinho <davidmartinho@gmail.com>
2011-07-07 15:31:24 +0100

    Added some Javadoc to describe the class

--- src/main/java/pt/ist/processpedia/client/dto/CreateGoalInputDto.java
+++ src/main/java/pt/ist/processpedia/client/dto/CreateGoalInputDto.java
@@ -1,19 +1,52 @@
+/**
+ * Copyright 2011 ESW Software Engineering Group
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 package pt.ist.processpedia.client.dto;
 
+/**
+ * A data container with the necessary input data to create a new goal.
+ */
 public class CreateGoalInputDto implements Dto {
-	
+
 	private String title;
 	private String description;
-		
+
+  /**
+   * Creates a new data container with the necessary input data.
+   * @param title the goal's title
+   * @param description the goal's description
+   */
 	public CreateGoalInputDto(String title, String description) {
 		this.title = title;
 		this.description = description;
 	}
-	
+
+  /**
+   * Obtain the title associated to the goal.
+   * @return the goal's title
+   */
 	public String getTitle() {
 		return this.title;
-	}                  
-	
+	}
+
+  /**
+   * Obtain the description associated to the goal.
+   * @return the goal's description
+   */
 	public String getDescription() {
 		return this.description;
 	}
