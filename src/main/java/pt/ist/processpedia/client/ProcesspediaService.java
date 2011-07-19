4c1c1aabbf8073d8277f028042d20796d461a7fa
David Martinho <davidmartinho@gmail.com>
2011-07-19 15:04:32 +0100

    Removed the GWT RPC services and some unused imports from the main Entrypoint

--- src/main/java/pt/ist/processpedia/client/ProcesspediaService.java
+++
@@ -1,9 +0,0 @@
-package pt.ist.processpedia.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("processpedia")
-public interface ProcesspediaService extends RemoteService {
-  public String greetServer(String name) throws IllegalArgumentException;
-}
